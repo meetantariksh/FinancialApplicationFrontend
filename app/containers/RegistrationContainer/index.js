edba7e3868a5afe8399aabb7c6687800bc8672a6
Antariksh <meetantariksh@gmail.com>
2017-12-16 23:08:58 +0530

    Registration_Setup

--- app/containers/RegistrationContainer/index.js
+++ app/containers/RegistrationContainer/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import { push } from 'react-router-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
@@ -24,19 +25,96 @@ import runMain from '../../externalJS/RegistrationPage/main';
 import runVelocity from '../../externalJS/RegistrationPage/velocity.min'; 
 
 export class RegistrationContainer extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  
+  constructor(props){
+    super(props);
+    this.goToHomePage = this.goToHomePage.bind(this);
+  }
+
   componentWillMount(){
     runMain();
     runVelocity();
   }
 
+  goToHomePage(){
+    this.props.dispatch(push('/'));
+  }
+
   render() {
     return (
-      <div>
+      <div className="body-class">
         <Helmet>
           <title>Arian Finance - Registration</title>
           <meta name="description" content="Description of RegistrationContainer" />
         </Helmet>
+        <header className="cd-main-header">
+          <h1 style={{paddingTop: '50px', paddingBottom: '25px'}}>Arian Finance Registration</h1>
+          <a onClick={this.goToHomePage}><h2>Return To Home Page</h2></a>
+        </header>
+        <div className="body-class">
+          <ul className="cd-pricing">
+            
+            <li style={{display: 'hide'}}>
+              <header className="cd-pricing-header">
+                <h2>General</h2>
+                <h2>Insurance</h2>
+              </header>
+
+              <div className="cd-pricing-features">
+                <ul>
+                  <li className="available"><em>Feature 1</em></li>
+                  <li className="available"><em>Feature 2</em></li>
+                  <li className="available"><em>Feature 3</em></li>
+                  <li className="available"><em>Feature 4</em></li>
+                </ul>
+              </div>
+
+              <footer className="cd-pricing-footer">
+                <a href="#0">Select</a>
+              </footer>
+            </li>
+
+            <li>
+              <header className="cd-pricing-header">
+                <h2>Mutual</h2>
+                <h2>Funds</h2>
+              </header>
+
+              <div className="cd-pricing-features">
+                <ul>
+                  <li className="available"><em>Feature 1</em></li>
+                  <li className="available"><em>Feature 2</em></li>
+                  <li className="available"><em>Feature 3</em></li>
+                  <li className="available"><em>Feature 4</em></li>
+                </ul>
+              </div>
+
+              <footer className="cd-pricing-footer">
+                <a href="#0">Select</a>
+              </footer>
+            </li>
+
+            <li>
+              <header className="cd-pricing-header">
+                <h2>Life</h2>
+                <h2>Insurance</h2>
+              </header>
+
+              <div className="cd-pricing-features">
+                <ul>
+                  <li className="available"><em>Feature 1</em></li>
+                  <li className="available"><em>Feature 2</em></li>
+                  <li className="available"><em>Feature 3</em></li>
+                  <li className="available"><em>Feature 4</em></li>
+                </ul>
+              </div>
+              <footer className="cd-pricing-footer">
+                <a href="#0">Select</a>
+              </footer>
+            </li>
+
+
+          </ul> 
+        </div>
       </div>
     );
   }
