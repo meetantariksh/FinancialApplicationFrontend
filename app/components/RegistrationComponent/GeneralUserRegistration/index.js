d712266d5a43ed3ef97b21fc62479be9a05a2b9e
meetantariksh <meetantariksh@gmail.com>
2017-12-23 00:34:20 +0530

    Registration Setup with back end

---
+++ app/components/RegistrationComponent/GeneralUserRegistration/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* GeneralUserRegistration
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class GeneralUserRegistration extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+GeneralUserRegistration.propTypes = {
+
+};
+
+export default GeneralUserRegistration;
